a6f04d3e4ef7235e1d9121ff19faf40de52e7e30
kevinberth <kevin.berth@me.com>
2020-10-09 11:15:18 +0200

    final change I think

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log(`Yes you can do it, you're not gonna have a lot of sparetime, but at the end you will have an awesome new skill!`)
+console.log(`Yes you can do it, you're not gonna have a lot of sparetime and it will be hard, but at the end you will have an awesome new skill!`)
